ce699b3e4d84ffa6888b27bbf639aaa051f62704
Hunter Haugen <hunter@haugens.org>
2025-05-10 12:27:02 -0700

    Add fava-dashboards

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build --build-arg="PIP_PKGS=beancount_reds_plugins" -t local/fava .
+docker build --build-arg="PIP_PKGS=beancount_reds_plugins git+https://github.com/andreasgerstmayr/fava-dashboards.git" -t local/fava .
